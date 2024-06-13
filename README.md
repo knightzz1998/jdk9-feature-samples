22786386007591fa60123c0f019fac3b982c09ff
王天赐 <wangtianci2068@gmail.com>
2024-06-13 16:49:32 +0800

    :fire: 函数式接口

---
+++ README.md
@@ -0,0 +1,5 @@
+## JDK 新特性
+
+## 函数式接口 functional-interface
+
+https://blog.csdn.net/qq877728715/article/details/106781098
---
+++ functional-interface/src/main/java/cn/knightzz/Main.java
@@ -0,0 +1,13 @@
+package cn.knightzz;
+
+/**
+ * @author 王天赐
+ * @title ${NAME}
+ * @description ${description}
+ * @create 2024-06-13 16:48
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
